f81e88a350b03f25fd23ae7077a15a0c610d664d
alexpetrov-intel <169180790+alexpetrov-intel@users.noreply.github.com>
2024-08-01 14:41:53 -0700

    working write to file

--- re3stat.c
+++ re3stat.c
@@ -1,7 +1,6 @@
 #define USE_THE_REPOSITORY_VARIABLE
 
 #include "builtin.h"
-#include "commit.h"
 #include "hex.h"
 #include "object-name.h"
 #include "pretty.h"
@@ -9,11 +8,6 @@
 #include "rerere.h"
 #include "revision.h"
 #include "strbuf.h"
-
-// #include "string-list.h"
-// #include "gettext.h"
-// #include "config.h"
-// #include "wt-status.h"
 
 struct rerere_dir {
 	int status_alloc, status_nr;
@@ -33,22 +27,26 @@ void save_rr_stats(const char *path, struct rerere_id *id)
 	printf("Is working on path: %s\n", path);
 
 	if(repo_get_oid(the_repository, "MERGE_HEAD", &oid) == 0) {
-		commitptr = lookup_commit(the_repository, &oid);
-		strbuf_reset(&prettybuf);
-		strbuf_addstr(&prettybuf, "\nFrom MERGE_HEAD: \n");
-		strbuf_addstr(&prettybuf, oid_to_hex(&oid));
-		strbuf_addstr(&prettybuf, " - ");
-		pp_commit_easy(CMIT_FMT_ONELINE, commitptr, &prettybuf);
-		puts(prettybuf.buf);
 
 		//write to file
-		printf("Writing current MERGE_HEAD to %s", rerere_path(id, "MERGES"));
-		f = fopen(path, "a");
-		if (!f){
+		printf("Writing current MERGE_HEAD to %s", rerere_path(id, "merge_list"));
+		f = fopen(rerere_path(id, "merge_list"), "a");
+		if (f){
+			commitptr = lookup_commit(the_repository, &oid);
+			strbuf_reset(&prettybuf);
+			pp_commit_easy(CMIT_FMT_ONELINE, commitptr, &prettybuf);
+
+			fputs(oid_to_hex(&oid), f);
+			fputs(" ", f);
+			fputs(prettybuf.buf, f);
+			fputs("\n", f);
+		} else {
 			printf("Could not open file");
 		}
+
+		fclose(f);
 	} else {
-		printf("MERGE_HEAD gave a yucky");
+		printf("No MERGE_HEAD");
 	}
 
 	if(repo_get_oid(the_repository, "ORIG_HEAD", &oid) == 0) {
@@ -61,9 +59,13 @@ void save_rr_stats(const char *path, struct rerere_id *id)
 		puts(prettybuf.buf);
 
 	} else {
-		printf("ORIG_HEAD gave a yucky");
+		printf("no ORIG_HEAD");
 	}
 
 	strbuf_release(&prettybuf);
 	printf("====================\n");
+}
+
+void write_commit(const char *path, const char *head_name, struct commit *commitptr) {
+
 }
--- re3stat.h
+++ re3stat.h
@@ -2,7 +2,10 @@
 #define RE3STAT_H
 
 #include "rerere.h"
+#include "commit.h"
+
 
 void save_rr_stats(const char *path, struct rerere_id *id);
+void write_commit(const char *path, const char *head_name, struct commit *commitptr);
 
 #endif
